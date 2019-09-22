d50e92d33979fa0f433de80aec54d1aee3ebdd93
MscBaiMeow <baishu2003@outlook.com>
2019-09-22 10:33:37 +0800

    输出优化

--- app.go
+++ app.go
@@ -83,18 +83,33 @@ func augur(fromQQ int64) int32 {
 	destiny := hash.Sum(nil) //获取destiny 天命
 	rand.Seed(int64(binary.BigEndian.Uint64(destiny)))
 
-	//占卜获得以太坐标
+	//占卜获得以太坐标与幸运指数
+	luckindex := rand.Intn(100) + 1
+	result := fmt.Sprintf("大佬今天的仙气值（1-100）为 %d\n", luckindex)
 	placeID := rand.Intn(len(config.Places))
-	result := "去" + config.Places[placeID].Name
 
 	//占卜具体细节
-	events := append(config.GeneralEvents, config.Places[placeID].PlaceEvents...)
-	e := events[rand.Intn(len(events))]
-
-	if e.Lucky {
-		result = "今日 吉：" + result + e.Name
-	} else {
-		result = "今日 凶：" + result + e.Name
+	goodevents := append(config.GoodEvents, config.Places[placeID].GoodEvents...)
+	badevents := append(config.BadEvents, config.Places[placeID].BadEvents...)
+	switch {
+	case luckindex <= 15:
+		result += "今日 凶\n"
+		result += "去" + config.Places[placeID].Name + badevents[rand.Intn(len(badevents))] + "\n"
+		result += "接受审判吧"
+	case luckindex > 15 && luckindex <= 45:
+		result += "今日 凶\n"
+		result += "去" + config.Places[placeID].Name + badevents[rand.Intn(len(badevents))] + "\n"
+		result += "不过呢" + badevents[rand.Intn(len(badevents))]
+	case luckindex > 45 && luckindex <= 55:
+		result += "今日 平，无特殊事件"
+	case luckindex >= 55 && luckindex <= 85:
+		result += "今日吉\n"
+		result += "去" + config.Places[placeID].Name + goodevents[rand.Intn(len(goodevents))] + "\n"
+		result += "但是要注意" + badevents[rand.Intn(len(badevents))]
+	case luckindex > 85:
+		result += "今日 吉\n"
+		result += "去" + config.Places[placeID].Name + goodevents[rand.Intn(len(goodevents))] + "\n"
+		result += "不会发生不幸的事情的"
 	}
 
 	cqp.SendGroupMsg(config.Group, result)
